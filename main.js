eb9f59c521638d47d2780e8f80d1ca683d4968bb
Isabela2808 <148455867+Isabela2808@users.noreply.github.com>
2025-03-24 07:44:28 -0300

    .

--- main.js
+++ main.js
@@ -37,7 +37,7 @@ function calculaTempo(tempoObjetivo) {
     if (tempoFinal > 0) {
         return [dias, horas, minutos, segundos];
     } else {
-        return [1, 2, 3, 4];
+        return [2, 8, 0, 8];
     }
 }
 
